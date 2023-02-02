fe4ee4d1b1c273f0d0e89c47ca29dce712ad2a8b
Vasilyev Maksim <jeckhummer@gmail.com>
2023-02-02 12:41:15 +0400

    Old files deleted

--- server.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import * as ReactDOMServer from "react-dom/server";
-import { Resume } from "./Resume";
-import { writeFileSync } from "fs";
-
-writeFileSync(
-  "index.html",
-  "<!DOCTYPE html>" + ReactDOMServer.renderToStaticMarkup(<Resume />)
-);
