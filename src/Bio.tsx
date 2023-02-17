65bb700fd9c7b5f97255d9ef2c1467dc25fc5218
Korovin Alexander <sanya.korovin@gmail.com>
2023-02-17 14:01:51 +0300

    Update Bio.tsx

--- src/Bio.tsx
+++ src/Bio.tsx
@@ -3,14 +3,12 @@ import * as React from 'react';
 export const Bio: React.FC = () => {
   return (
     <p className="bio">
-      Product Manager with 8+ years of experience in product focused companies and consulting, working for global brands in banking, insurance, e-commerce, and energy. 
+      Product Manager with 8+ years of experience in product focused companies and consulting, working for global brands in banking, insurance, e-commerce, and energy.
+      Have a founder’s mentality and strong skillset across product management domain: developed and implemented digital transformation strategies, prepared financial models and budgets, managed teams, created new products and features from idea to launch, 
       <br />
       <br />
-      Have a founder’s mentality and strong skillset across product management domain, developed and implemented digital transformation strategies, prepared financial models and budgets, managed teams, created new products and features from idea to launch, 
-      developed machine learning systems for enterprises
-      <br />
-      <br />
-      Prior experience includes management and digital consulting (Deloitte Digital, KPMG), banking and risk management. Significant international experience and ability to work in a multi-national and multi-cultural environment (lived for 2 years in CIS, US, and EU, maintain regular contacts with a global network)
+      Prior experience includes management and digital consulting (Deloitte Digital, KPMG), banking and risk management. 
+      Experienced in managing scrum teams (5-10 people) and communicating with top senior stakeholders
       <br />
       <br />
       Anabin certified university and specialisation: Bachelor’s degree in Applied mathematics; Master in GSOM business school and University of St. Gallen (ranked #14 by the Financial Times globally)
