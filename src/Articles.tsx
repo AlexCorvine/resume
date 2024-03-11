c7e2bda2193eba3d333279ad95f29ef629f3f387
Korovin Alexander <sanya.korovin@gmail.com>
2024-03-11 22:43:10 +0100

    Update Articles.tsx

--- src/Articles.tsx
+++ src/Articles.tsx
@@ -8,15 +8,14 @@ export const Articles: React.FC = () => {
       <BulletedList
         bullets={[
           {
-            label: "Product storytelling using Gamification principles",
-            href: "https://alexkorvin.medium.com/storytelling-3-0-gamification-strategies-79160e670b0b",
+            label: "Guest lecturer on product management in British Higher School of Art & Design and PWC",
           },
           {
             label: "Speaker in Product Hunt 2022, Baku conference - Product storytelling ",
             href: "https://www.youtube.com/live/ObquU5MwrKM?feature=share&t=10023",
           },
           {
-            label: "Storytelling and Presentation design (RU), open webinar to RANEPA master program: Digital product and visualisation",
+            label: "Launched product storytelling course. Conducted of 10+ trainings in EPAM, IBM and сlients",
             href: "https://ion.ranepa.ru/news/storytelling-i-oformlenie-prezentatsiy-vebinar-aleksandra-korovina-ibm-i-alekseya-mordasova-sberbank/",
           },
         ]}
